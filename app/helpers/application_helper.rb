2d79d14e2985c83f2e6bf09751c4f8ec5b8c9c9d
John Lewis <dragon99x@hotmail.com>
2014-11-17 20:07:44 -0600

    Updated the flash messaging services.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-success"
+      when :warn then "alert alert-warning"
+      when :info then "alert alert-info"
+      else "alert alert-danger"
+    end
+  end
 end
